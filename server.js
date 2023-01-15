73ec97c8e61b360861c5dff509782b946ab883c9
janghyeok <janghyeok9257@gmail.com>
2023-01-15 23:21:20 +0900

    Create Movie Query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
   {
@@ -11,25 +12,75 @@ let tweets = [
   },
 ];
 
+let users = [
+  {
+    id: "1",
+    firstName: "janghyeok",
+    lastName: "cho",
+  },
+  {
+    id: "2",
+    firstName: "123",
+    lastName: "333",
+  },
+];
+
 const typeDefs = gql`
+  type Query {
+    allMovies: [Movie!]!
+    allUsers: [User!]!
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+    movie(id: String!): Movie
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
+    deleteTweet(id: ID!): Boolean!
+  }
   type User {
     id: ID!
     username: String!
     firstName: String!
     lastName: String!
+    """
+    Is the sum of firstName + lastName as a string
+    """
+    fullName: String!
   }
+  """
+  Tweet object repersents a resource for a tweet
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User
   }
-  type Query {
-    allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet
-  }
-  type Mutation {
-    postTweet(text: String!, userId: ID!): Tweet!
-    deleteTweet(id: ID!): Boolean!
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_fill: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -42,12 +93,26 @@ const resolvers = {
       const tweet = tweets.find((tweet) => tweet.id === id);
       return tweet;
     },
+    allUsers() {
+      return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((response) => response.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((response) => response.json())
+        .then((json) => json.data.movie);
+    },
   },
   Mutation: {
     postTweet(_, { text, userId }) {
       const newTweet = {
         id: tweets.length + 1,
         text,
+        userId,
       };
       tweets.push(newTweet);
       return newTweet;
@@ -57,6 +122,16 @@ const resolvers = {
       if (!tweet) return false;
       tweets = tweets.filter((t) => t.id !== id);
       return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((item) => item.id === userId);
     },
   },
 };
